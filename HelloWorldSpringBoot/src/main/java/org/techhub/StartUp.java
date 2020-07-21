287bb8e42e9308e4cc31cabdcd38e711169dd3ea
NAITIK <naitikgiri23@gmail.com>
2020-07-21 17:13:19 +0530

    This is the First Hello World Application Using Spring Boot

---
+++ HelloWorldSpringBoot/src/main/java/org/techhub/StartUp.java
@@ -0,0 +1,15 @@
+package org.techhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
---
+++ HelloWorldSpringBoot/src/main/java/org/techhub/controller/TestController.java
@@ -0,0 +1,14 @@
+package org.techhub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/")
+	public String homePage()
+	{
+		return "Welcome on Spring Boot Application";
+	}
+}
